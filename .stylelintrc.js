1b3861b343bc213370a4e9c10662f4e2d7c9665d
kaave <abe@framelunch.jp>
2017-04-10 14:19:01 +0900

    Add stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: 'stylelint-config-sass-guidelines',
+  ignoreFiles: [
+    'node_modules/**/*'
+  ],
+  /*
+   * 以下に緩めたいルールを記述していく。
+   * 参考: https://github.com/bjankord/stylelint-config-sass-guidelines
+   */
+  rules: {
+    // インデントは4
+    indentation: 4,
+    // 文字列はシングルでもダブルでも構わない
+    'string-quotes': null,
+    // 色指定は小文字大文字どちらでもOK
+    'color-hex-case': null,
+    // ネストの深さはこんくらい？
+    'max-nesting-depth': 5,
+    // BEMにマッチさせるのが難しいので無効
+    'selector-class-pattern': null,
+    // アルファベット順に並べましょう、だなんて面倒だし意味ごとにまとめたいこともあるので無効
+    'order/declaration-block-properties-alphabetical-order': null
+  }
+};
